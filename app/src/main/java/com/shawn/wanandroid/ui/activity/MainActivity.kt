cde32d4466d4a01ca5fdeba414e179ef5ea01906
SHLURENJIA <SH_LURENJIA@163.com>
2019-10-12 10:31:53 +0800

    添加MD控件，资源文件

---
+++ app/src/main/java/com/shawn/wanandroid/ui/activity/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.shawn.wanandroid.ui.activity
+
+import com.shawn.wanandroid.R
+import com.shawn.wanandroid.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun attachLayoutRes(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initListener() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
